8aa0c77f20beeef0a1eb1e39ae16aebe483b71a5
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-03-12 00:37:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
-model = pickle.load(open('model.pickle', "rb"))
+filename = 'model.pickle'
+model = pickle.load(open(filename, "rb"))
 st.title("Revenue Prediction")
 a = st.number_input('Input Temperature')
 if st.button('Predict'):
