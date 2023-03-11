5a1664e449d6ae13875eda541ba485299bd9eedc
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-03-12 00:47:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ st.title("Revenue Prediction")
 a = st.number_input('Input Temperature')
 if st.button('Predict'):
   result = model.predict(a)
+  st.text('Revenue Prediction')
   st.success(str(result))
