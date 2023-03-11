82e9c8271ca0f79acc24cf2950ee0f5d9b576bed
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-03-11 23:55:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pickle
-pickle.dump(model, open(filename, "wb"))
 model = pickle.load(open('model.pickle', "rb"))
 st.title("Revenue Prediction")
 a = st.number_input('Input Temperature')
