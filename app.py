0366a009f36f3e4919a3e8f8aec692614f64b8b7
phamtin13 <125672353+phamtin13@users.noreply.github.com>
2023-03-12 12:31:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ filename = 'model.pickle'
 model = pickle.load(open(filename, "rb"))
 st.title("Revenue Prediction")
 a = st.number_input('Input Temperature')
+a = a.reshape(-1,1)
 if st.button('Predict'):
   result = model.predict(a)
   st.caption('Revenue Prediction')
